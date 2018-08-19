e7c8b289e1a2c54d3cd432a7000d501ffe8c1103
Vijay Kumar <vijay@ourjamz.com>
2018-08-18 23:55:16 -0700

    permutation

---
+++ permutation.py
@@ -0,0 +1,20 @@
+# Permutation
+
+
+def permutation_rec(a):
+    if len(a) == 0:
+        return []
+    if len(a) == 1:
+        return [a]
+
+    l = []
+    for i in range(len(a)):
+        x = a[:i] + a[i+1:]
+        for j in permutation_rec(x):
+            str2 = a[i] + j
+            l.append(str2)
+    return l
+
+
+for r in permutation_rec('abcd'):
+    print(r)
