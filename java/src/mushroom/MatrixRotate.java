ad9403349fbc6b90fb48b8d8c5a8f5cb45c7d9d8
Vijay Kumar <vijay@ourjamz.com>
2018-09-06 21:13:36 -0700

    Format

--- java/src/mushroom/MatrixRotate.java
+++ java/src/mushroom/MatrixRotate.java
@@ -4,49 +4,46 @@ package mushroom;
 
 public class MatrixRotate {
 	// Rotate a square clockwise
-	public void rotate(int[][]a) {
+	public void rotate(int[][] a) {
 		int n = a.length;
 		// Transpose
-		for (int i=0; i < n; i++) {
-			for (int j=i; j < n; j++ ) {
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
 				int temp = a[i][j];
 				a[i][j] = a[j][i];
-				a[j][i] = temp;						
+				a[j][i] = temp;
 			}
 		}
 		System.out.println("After Transposing");
 		print(a);
 		// reverse rows
-		for (int i=0; i < n; i++) {
-			for (int j=0; j < n/2; j++ ) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n / 2; j++) {
 				int temp = a[i][j];
-				a[i][j] = a[i][n-1-j];
-				a[i][n-1-j] = temp;						
+				a[i][j] = a[i][n - 1 - j];
+				a[i][n - 1 - j] = temp;
 			}
 		}
 	}
-	
-	public void print(int[][]a) {
+
+	public void print(int[][] a) {
 		int n = a.length;
-		for (int i=0; i < n; i++) {
-			for (int j=0; j < n; j++ ) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
 				System.out.print(a[i][j] + ", ");
 			}
 			System.out.println("");
 		}
 		System.out.println("");
 	}
+
 	public static void main(String[] args) {
 		var m = new MatrixRotate();
-		var a = new int [][] {
-			{1,2,3,4},
-			{5,6,7,8},
-			{9,10,11,12},
-			{13,14,15,16}
-			
+		var a = new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 }
+
 		};
 		m.print(a);
 		m.rotate(a);
-		m.print(a);		
+		m.print(a);
 	}
 }
